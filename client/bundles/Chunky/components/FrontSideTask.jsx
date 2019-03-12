d049b264c33796c9333c8952723b4e87559e519d
James Harris <computersherpa@gmail.com>
2019-03-11 19:27:05 -0500

    Indentation

--- client/bundles/Chunky/components/FrontSideTask.jsx
+++ client/bundles/Chunky/components/FrontSideTask.jsx
@@ -6,9 +6,7 @@ import Checkbox from './Checkbox';
 import send from './send';
 import deleteTask from './deleteTask';
 
-/**
- * This component is placed either on a BackSideTask component or in an Outline.
- */
+// This component is placed either on a BackSideTask component or in an Outline.
 export default class FrontSideTask extends React.Component {
 	static propTypes = {
 		task: PropTypes.object.isRequired,
@@ -29,7 +27,7 @@ export default class FrontSideTask extends React.Component {
 	handleCheckbox(event) {
 		this.checkboxChange(event, this);
 	}
-
+	
 	// If FrontSideTask is in a BackSideTask, BackSideTask will provide a checkboxChange function. If not, we use this one.
 	checkboxChange(event) {
 		const completed = event.target.checked;
@@ -41,31 +39,32 @@ export default class FrontSideTask extends React.Component {
 				}
 			}),
 			() => { send(this.state.task); }
-		);
-	}
-
-	render() {
-		var url = "/tasks/" + this.state.task.id;
-		return (
-			<div className="task-card-front">
+			);
+		}
+		
+		render() {
+			var url = "/tasks/" + this.state.task.id;
+			return (
+				<div className="task-card-front">
 				{/* <button className="delete-attachment-button" onClick={this.deleteTask}><Icon.Trash2 size="16" /></button> */}
 				<Checkbox checked={this.state.task.completed} handleChange={this.handleCheckbox} />
 				<a className="task-link" href={url}>{ this.state.task.name }</a>
 				<div className="details">
-					{ this.state.task.due_date ?
-						<div><Icon.Calendar size="16" /> {this.state.task.dueDate}</div> : ""
-					}
-					{ this.state.task.description ?
-						<div><Icon.AlignLeft size="16" /></div> : ""
-					}
-					{ this.state.task.attachment_count ?
-						<div><Icon.Paperclip size="16" /> {this.state.task.attachment_count}</div> : ""
-					}
-					{ parseInt(this.state.task.descendants.length) > 0 && !this.props.disableDescendantCount ? 
-						<div><Icon.CheckSquare size="16" /> {this.state.task.completed_descendants.length}/{this.state.task.descendants.length}</div> : ""
-					}
+				{ this.state.task.due_date ?
+					<div><Icon.Calendar size="16" /> {this.state.task.dueDate}</div> : ""
+				}
+				{ this.state.task.description ?
+					<div><Icon.AlignLeft size="16" /></div> : ""
+				}
+				{ this.state.task.attachment_count ?
+					<div><Icon.Paperclip size="16" /> {this.state.task.attachment_count}</div> : ""
+				}
+				{ parseInt(this.state.task.descendants.length) > 0 && !this.props.disableDescendantCount ? 
+					<div><Icon.CheckSquare size="16" /> {this.state.task.completed_descendants.length}/{this.state.task.descendants.length}</div> : ""
+				}
 				</div>
-			</div>
-		)
-	}
-}
+				</div>
+				)
+			}
+		}
+		
