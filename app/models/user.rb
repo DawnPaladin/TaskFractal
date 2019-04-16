94da6c48dad4e80a36ea846eab6e92437cec35f0
James Harris <computersherpa@gmail.com>
2019-04-15 21:09:41 -0500

    Remove :recoverable and :confirmable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :rememberable, :validatable
   
   has_many :tasks
 end
