8a7d3225f7ee67a85b339a3ac0ac5b62e9f53fd3
James Harris <computersherpa@gmail.com>
2019-04-27 23:41:35 -0500

    Basic drag-and-drop support on Outline

--- client/bundles/Chunky/components/Outline.jsx
+++ client/bundles/Chunky/components/Outline.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactOnRails from 'react-on-rails';
 import * as Icon from 'react-feather';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import FrontSideTask from './FrontSideTask';
 
@@ -56,17 +56,30 @@ export default class Outline extends React.Component {
 	onDragEnd = result => {
 		
 	}
-	taskTree(task) {
-		var children = task.descendants ? task.descendants.map(child => this.taskTree(child)) : <div></div>;
-		return (<div key={task.id}>
-			<FrontSideTask task={task} key={task.id} disableDescendantCount={true} />
-			<div className="indent">{ children }</div>
-		</div>)
+	taskTree(task, index) {
+		var children = task.descendants ? task.descendants.map((child, index) => this.taskTree(child, index)) : <div></div>;
+		return (
+			<Draggable draggableId={task.id} key={task.id} index={index}>
+				{(provided) => (
+					<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+						<FrontSideTask task={task} key={task.id} disableDescendantCount={true} />
+						<div className="indent">{ children }</div>
+					</div>
+				)}
+			</Draggable>
+		)
 	}
 	render() {
 		return <div className="outline">
 			<DragDropContext onDragEnd={this.onDragEnd}>
-				{ this.state.tasks.map(task => this.taskTree(task)) }
+				<Droppable droppableId="1">
+					{(provided) => (
+						<div ref={provided.innerRef} {...provided.droppableProps}>
+							{ this.state.tasks.map((task, index) => this.taskTree(task, index)) }
+							{ provided.placeholder }
+						</div>
+					)}
+				</Droppable>
 				<form className="task-adder" onSubmit={this.addNewTask} >
 					<input type="text" placeholder="New task" value={this.state.new_task_name} onChange={this.handleAddNewTaskEdit} />
 					<button>Add</button>
