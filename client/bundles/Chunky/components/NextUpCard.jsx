35fc1bbe33b2f39e7f8773627ad3d77076a81450
James Harris <computersherpa@gmail.com>
2019-05-25 14:05:28 -0500

    Format Next Up reasons

--- client/bundles/Chunky/components/NextUpCard.jsx
+++ client/bundles/Chunky/components/NextUpCard.jsx
@@ -7,8 +7,18 @@ export default class NextUpCard extends React.Component {
 	static propTypes = {
 		task: PropTypes.object,
 	}
+	constructor(props) {
+		super(props);
+	}
+	formatReasons = () => {
+		var array = this.props.task && this.props.task.reasons ? this.props.task.reasons : [];
+		if (array.length === 0) return "";
+		var string = array.join('; ');
+		var capitalizedString = string[0].toUpperCase() + string.slice(1);
+		return capitalizedString;
+	}
 	render() {
-		var reasons = this.props.task && this.props.task.reasons ? this.props.task.reasons : null;
+		var reasons = this.formatReasons();
 		return <div className="next-up-card">
 			{ this.props.task ? <FrontSideTask task={this.props.task} /> : null }
 			{/* FIXME: Give FrontSideTask a guard clause, causing it to render nothing if a task isn't passed? */}
