de6183174e841a6d801c69a175ff035b1d6091e6
James Harris <computersherpa@gmail.com>
2019-07-22 22:42:04 -0500

    Use Material Design checkbox

--- client/bundles/Chunky/components/Checkbox.jsx
+++ client/bundles/Chunky/components/Checkbox.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import MtlCheckbox from '@material/react-checkbox';
 
 export default class Checkbox extends React.Component {
 	static propTypes = {
@@ -7,6 +8,11 @@ export default class Checkbox extends React.Component {
 		checked: PropTypes.bool.isRequired,
 	};
 	render() {
-		return <input type="checkbox" onChange={this.props.handleChange} checked={!!this.props.checked} />
+		// return <input type="checkbox" onChange={this.props.handleChange} checked={!!this.props.checked} />
+		return <MtlCheckbox
+				checked={this.props.checked}
+				onChange={this.props.handleChange}
+			/>
+		// </React.Fragment>
 	}
 }
