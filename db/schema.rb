12507cc92593044de2e2c4054a2825b5646feb46
James Harris <computersherpa@gmail.com>
2019-10-06 16:36:32 -0500

    Change default theme to "Jeans"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_035418) do
+ActiveRecord::Schema.define(version: 2019_10_06_213353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2019_08_08_035418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "theme", default: "twilight-sparkle"
+    t.string "theme", default: "jeans"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
