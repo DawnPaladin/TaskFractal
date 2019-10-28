16c0eb12a2f2be1f936f26488a6dff793da5d8f7
James Harris <computersherpa@gmail.com>
2019-10-28 14:43:20 -0500

    Remove "Hello World" code

--- README.md
+++
@@ -1,1 +0,0 @@
-Start dev server with "npm run dev", then visit http://localhost:3000/hello_world. JSX is in app/javascript/bundles/HelloWorld/components/HelloWorld.jsx.
--- client/packs/bundles.js
+++ client/packs/bundles.js
@@ -4,7 +4,6 @@ import FrontSideTask from '../bundles/Chunky/components/FrontSideTask'
 import BackSideTask from '../bundles/Chunky/components/BackSideTask';
 import Outline from '../bundles/Chunky/components/Outline';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
 	BackSideTask, Outline, FrontSideTask
 });
