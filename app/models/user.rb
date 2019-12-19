379bc76d43f0240a1fdd15d6a61a2e4569e5d4fb
James Harris <computersherpa@gmail.com>
2019-12-19 17:34:27 -0600

    Design reset-password screen; simplify CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
-	devise :database_authenticatable, :registerable,
-				 :rememberable, :validatable
+	# :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+	devise :database_authenticatable, :registerable, :rememberable, :validatable, :recoverable
 	
 	has_many :tasks
 	
