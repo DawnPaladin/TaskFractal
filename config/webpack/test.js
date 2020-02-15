b88189363407452c154a3bbd14f6781244ed9a8e
James Harris <computersherpa@gmail.com>
2020-02-14 21:14:02 -0600

    Fix error: "Your test suite must contain at least one test"

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+test('1 + 1 equals 2', () => {
+	expect(1 + 1).toBe(2);
+});
