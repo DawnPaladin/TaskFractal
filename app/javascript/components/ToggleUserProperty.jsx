df0b182df03b903775edecf90034267ec0e261c3
James Harris <computersherpa@gmail.com>
2020-03-20 15:14:58 -0500

    Rewrite ToggleUserProperty to a functional component that uses hooks

--- app/javascript/components/ToggleUserProperty.jsx
+++ app/javascript/components/ToggleUserProperty.jsx
@@ -1,46 +1,43 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Icon from 'react-feather';
 import network from './network';
 
-export default class ToggleUserProperty extends React.Component {
-	static propTypes = {
-		property: PropTypes.string.isRequired, // example: 'completed_tasks_visible'
-		label: PropTypes.string.isRequired,
-		initialState: PropTypes.bool.isRequired,
-		userId: PropTypes.number.isRequired,
-		networkPath: PropTypes.string.isRequired, // API endpoint to hit. Example: '/change_completed_tasks_visible.json'
-		eventName: PropTypes.string, // Event to broadcast on toggle. Example: 'toggleShowCompleted'
-		altKey: PropTypes.string // example: "c" for alt-C
-	};
-	constructor(props) {
-		super(props);
-		this.state = { enabled: this.props.initialState }
-		this.handleChange = this.handleChange.bind(this);
-	}
-	handleChange = () => {
-		const newState = !this.state.enabled;
-		this.setState({ enabled: newState });
-		network.patch(this.props.networkPath, { [this.props.property]: newState });
-		if (this.props.eventName) {
-			const event = new CustomEvent(this.props.eventName, { detail: newState });
+export default function ToggleUserProperty(props) {
+	const [enabled, setEnabled] = useState(props.initialState);
+	
+	const handleChange = () => {
+		const newState = !enabled;
+		setEnabled(newState);
+		network.patch(props.networkPath, { [props.property]: newState });
+		if (props.eventName) {
+			const event = new CustomEvent(props.eventName, { detail: newState });
 			document.dispatchEvent(event);
 		}
 	}
-	keyboardShortcut = event => {
-		if (event.altKey && event.key === this.props.altKey) {
-			this.handleChange();
+	const checkKeyboardShortcut = event => {
+		if (event.altKey && event.key === props.altKey) {
+			handleChange();
 		}
 	}
-	componentDidMount() {
-		document.addEventListener("keydown", this.keyboardShortcut);
-	}
-	render() {
-		const keyboardShortcut = this.props.altKey ? "(Alt-" + this.props.altKey.toUpperCase() + ")" : null;
-		return <button className="toggle-user-property-button" onClick={this.handleChange}>
-			{ this.state.enabled === true && <Icon.ToggleRight size="16" className="feather switch-on"  /> }
-			{ this.state.enabled === false && <Icon.ToggleLeft size="16" className="feather switch-off" /> }
-			{ this.props.label } <small>{keyboardShortcut}</small>
-		</button>
-	}
+	useEffect(() => {
+		document.addEventListener("keydown", checkKeyboardShortcut);
+	})
+
+	const keyboardShortcut = props.altKey ? "(Alt-" + props.altKey.toUpperCase() + ")" : null;
+	return <button className="toggle-user-property-button" onClick={handleChange}>
+		{ enabled === true && <Icon.ToggleRight size="16" className="feather switch-on"  /> }
+		{ enabled === false && <Icon.ToggleLeft size="16" className="feather switch-off" /> }
+		{ props.label } <small>{keyboardShortcut}</small>
+	</button>
+}
+
+ToggleUserProperty.propTypes = {
+	property: PropTypes.string.isRequired, // example: 'completed_tasks_visible'
+	label: PropTypes.string.isRequired,
+	initialState: PropTypes.bool.isRequired,
+	userId: PropTypes.number.isRequired,
+	networkPath: PropTypes.string.isRequired, // API endpoint to hit. Example: '/change_completed_tasks_visible.json'
+	eventName: PropTypes.string, // Event to broadcast on toggle. Example: 'toggleShowCompleted'
+	altKey: PropTypes.string // example: "c" for alt-C
 }
